d805d253433857fded730a707c79e3f6840969c9
MindaugasSimkus <shimkus.mindaugas@gmail.com>
2017-07-03 20:25:22 +0300

    added _SESSION variable

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
+//sesijos kintamieji
+
+session_start();
+
 $title = "Templates";
 $content = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -7,9 +11,20 @@ quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-$templates = ["green", "orange", "pink",];
+$templates = ["green", "orange", "pink", "yellow",];
 
-$template_name = $_GET['t'];
+
+
+
+if (isset($_GET['t'])) {
+	$template_name = $_GET['t'];
+	$_SESSION['session_template'] = $_GET['t'];
+} else if ($_SESSION['session_template']) {
+	$template_name = $_SESSION['session_template'];
+} else {
+	$template_name = $templates[0];
+}
+
 
 include 'templates/' . $template_name . '.php';
 
